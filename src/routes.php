1a16b9bcebe1b3d3d831c451c22814021f41fc86
acr <acarbey15@gmail.com>
2018-01-18 20:24:13 +0300

    yaz

--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,12 @@
 Route::group(['middleware' => ['web']], function () {
     Route::group(['namespace' => 'Acr\Acr_blog\Controllers', 'prefix' => 'acr/blog'], function () {
         Route::get('/', 'BlogController@blog_galery');
+        Route::get('/oku', 'BlogController@blog_oku');
         Route::group(['middleware' => ['auth']], function () {
             Route::group(['middleware' => ['admin']], function () {
                 Route::get('/list', 'BlogController@blog');
                 Route::get('/yeni', 'BlogController@yeni');
-                Route::get('/oku', 'BlogController@blog_oku');
+
                 Route::post('/create', 'BlogController@create');
                 Route::post('/delete', 'BlogController@delete');
                 Route::post('/file/delete', 'BlogController@file_delete');
